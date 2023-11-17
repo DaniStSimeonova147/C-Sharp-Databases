23367aed9d8a3856922767821e5d9408541b8582
Dani Simeonova <danisimeonova147@gmail.com>
2023-11-17 09:22:22 +0200

    Add MyMigration

---
+++ Entity Framework Core/04.CODE-FIRST/CodeFirst/P01_HospitalDatabase/Migrations/20191102201658_MyMigration.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P01_HospitalDatabase.Migrations
+{
+    public partial class MyMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Visitations_Patients_VisitationId",
+                table: "Visitations");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VisitationId",
+                table: "Visitations",
+                nullable: false,
+                oldClrType: typeof(int))
+                .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DoctorId",
+                table: "Visitations",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Doctors",
+                columns: table => new
+                {
+                    DoctorId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 100, nullable: false),
+                    Specialty = table.Column<string>(maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Doctors", x => x.DoctorId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Visitations_DoctorId",
+                table: "Visitations",
+                column: "DoctorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Visitations_PatientId",
+                table: "Visitations",
+                column: "PatientId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Visitations_Doctors_DoctorId",
+                table: "Visitations",
+                column: "DoctorId",
+                principalTable: "Doctors",
+                principalColumn: "DoctorId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Visitations_Patients_PatientId",
+                table: "Visitations",
+                column: "PatientId",
+                principalTable: "Patients",
+                principalColumn: "PatientId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Visitations_Doctors_DoctorId",
+                table: "Visitations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Visitations_Patients_PatientId",
+                table: "Visitations");
+
+            migrationBuilder.DropTable(
+                name: "Doctors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Visitations_DoctorId",
+                table: "Visitations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Visitations_PatientId",
+                table: "Visitations");
+
+            migrationBuilder.DropColumn(
+                name: "DoctorId",
+                table: "Visitations");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VisitationId",
+                table: "Visitations",
+                nullable: false,
+                oldClrType: typeof(int))
+                .OldAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Visitations_Patients_VisitationId",
+                table: "Visitations",
+                column: "VisitationId",
+                principalTable: "Patients",
+                principalColumn: "PatientId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
