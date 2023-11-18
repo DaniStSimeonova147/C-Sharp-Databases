ff9b58140d8e9858699213c62203e4d918ad8bbf
Dani Simeonova <danisimeonova147@gmail.com>
2023-11-18 11:02:34 +0200

    Add entity relations models and migrations

---
+++ Entity Framework Core/05.ENTITY RELATIONS/P01_StudentSystem/Data/Configuration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_StudentSystem.Data
+{
+   public static class Configuration
+    {
+        public static string ConnectionString = @"Server=LAPTOP-SELJOP4P\SQLEXPRESS;Database=StudentSystem;Integrated Security=true;";
+    }
+}
