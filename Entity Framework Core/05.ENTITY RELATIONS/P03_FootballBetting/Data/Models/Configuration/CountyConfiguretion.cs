6c758beca5d96c1e23fc9774454e0573cb5129b6
Dani Simeonova <danisimeonova147@gmail.com>
2023-11-18 11:09:19 +0200

    Add country configuration

---
+++ Entity Framework Core/05.ENTITY RELATIONS/P03_FootballBetting/Data/Models/Configuration/CountyConfiguretion.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using P03_FootballBetting.Data.Models;
+
+namespace P03_FootballBetting.Data.Configurations
+{
+  public  class CountyConfiguretion : IEntityTypeConfiguration<Country>
+    {
+        public void Configure(EntityTypeBuilder<Country> builder)
+        {
+            builder.HasKey(c => c.CountryId);
+
+            builder.Property(c => c.Name)
+                .HasMaxLength(60)
+                .IsRequired(true)
+                .IsUnicode(true);
+        }
+    }
+}
