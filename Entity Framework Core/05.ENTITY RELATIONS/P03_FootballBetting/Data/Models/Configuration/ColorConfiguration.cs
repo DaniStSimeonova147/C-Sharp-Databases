a8d3e5f68291e6b1cf15927c37138c732e0c9e8e
Dani Simeonova <danisimeonova147@gmail.com>
2023-11-18 11:08:34 +0200

    Add color configuration

---
+++ Entity Framework Core/05.ENTITY RELATIONS/P03_FootballBetting/Data/Models/Configuration/ColorConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using P03_FootballBetting.Data.Models;
+
+namespace P03_FootballBetting.Data.Configurations
+{
+    public class ColorConfiguration : IEntityTypeConfiguration<Color>
+    {
+        public void Configure(EntityTypeBuilder<Color> builder)
+        {
+            builder.HasKey(c => c.ColorId);
+
+            builder.Property(c => c.Name)
+                .HasMaxLength(30)
+                .IsRequired(true)
+                .IsUnicode(true);
+        }
+    }
+}
