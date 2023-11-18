71627254652a4b4b01b90fe917bc8b66920fb333
Dani Simeonova <danisimeonova147@gmail.com>
2023-11-18 11:06:51 +0200

    Add user configuration

---
+++ Entity Framework Core/05.ENTITY RELATIONS/P03_FootballBetting/Data/Models/Configuration/UserConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using P03_FootballBetting.Data.Models;
+
+
+namespace P03_FootballBetting.Data.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(u => u.UserId);
+
+            builder.Property(u => u.Username)
+                .HasMaxLength(60)
+                .IsRequired(true)
+                .IsUnicode(true);
+
+            builder.Property(u => u.Password)
+                .HasMaxLength(30)
+                .IsRequired(true)
+                .IsUnicode(true);
+
+            builder.Property(u => u.Email)
+                .HasMaxLength(30)
+                .IsRequired(true)
+                .IsUnicode(true);
+
+            builder.Property(u => u.Name)
+                .HasMaxLength(60)
+                .IsRequired(true)
+                .IsUnicode(true);
+
+            builder.Property(u => u.Balance)
+                .IsRequired(true);
+        }
+    }
+}
