c366720d6cbd6d77ab164a66bf4a60a5e2009249
Dani Simeonova <danisimeonova147@gmail.com>
2023-11-18 11:09:55 +0200

    Add Db context configuration

---
+++ Entity Framework Core/05.ENTITY RELATIONS/P03_FootballBetting/Data/Models/Configuration/DbContextConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace P03_FootballBetting.Data.Configurations
+{
+    public static class DbContextConfiguration
+    {
+        public static string ConnectionString = @"Server=LAPTOP-SELJOP4P\SQLEXPRESS;Database=FootballBetting;Integrated Security=true;";
+    }
+}
