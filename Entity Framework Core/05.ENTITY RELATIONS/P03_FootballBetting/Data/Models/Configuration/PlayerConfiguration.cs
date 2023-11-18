8340a1bae8917301f0c082ded445d00bdf3abda5
Dani Simeonova <danisimeonova147@gmail.com>
2023-11-18 11:11:01 +0200

    Add player configuration

---
+++ Entity Framework Core/05.ENTITY RELATIONS/P03_FootballBetting/Data/Models/Configuration/PlayerConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using P03_FootballBetting.Data.Models;
+
+namespace P03_FootballBetting.Data.Configurations
+{
+    public class PlayerConfiguration : IEntityTypeConfiguration<Player>
+    {
+        public void Configure(EntityTypeBuilder<Player> builder)
+        {
+            builder.HasKey(p => p.PlayerId);
+
+            builder.Property(p => p.Name)
+                .HasMaxLength(80)
+                .IsRequired(true)
+                .IsUnicode(true);
+
+            builder.Property(p => p.SquadNumber)
+                .IsRequired(true);
+
+            builder.Property(p => p.IsInjured)
+                .HasDefaultValue(false)
+                .IsRequired(true);
+
+            builder.Property(p => p.TeamId)
+                .IsRequired(true);
+
+            builder
+                .HasOne(p => p.Team)
+                .WithMany(t => t.Players)
+                .HasForeignKey(p => p.TeamId);
+
+            builder.Property(p => p.PositionId)
+                .IsRequired(true);
+
+            builder
+                .HasOne(p => p.Position)
+                .WithMany(ps => ps.Players)
+                .HasForeignKey(p => p.PositionId);
+        }
+    }
+}
