236a7313d0cb1b0dda2600030cc64039013e4733
Dani Simeonova <danisimeonova147@gmail.com>
2023-11-18 11:12:42 +0200

    Add team configuration

---
+++ Entity Framework Core/05.ENTITY RELATIONS/P03_FootballBetting/Data/Models/Configuration/TeamConfiguration.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using P03_FootballBetting.Data.Models;
+
+namespace P03_FootballBetting.Data.Configurations
+{
+    public class TeamConfiguration : IEntityTypeConfiguration<Team>
+    {
+        public void Configure(EntityTypeBuilder<Team> builder)
+        {
+            builder.HasKey(t => t.TeamId);
+
+            builder.Property(t => t.Name)
+                .HasMaxLength(80)
+                .IsRequired(true)
+                .IsUnicode(true);
+
+            builder.Property(t => t.Initials)
+                .HasMaxLength(3)
+                .IsRequired(true)
+                .IsUnicode(true);
+
+            builder.Property(t => t.Budget)
+                .IsRequired(true);
+
+            builder.Property(t => t.PrimaryKitColorId)
+                .IsRequired(true);
+
+            builder
+                .HasOne(t => t.PrimaryKitColor)
+                .WithMany(pk => pk.PrimaryKitTeams)
+                .HasForeignKey(t => t.PrimaryKitColorId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Property(t => t.SecondaryKitColorId)
+                .IsRequired(true);
+
+            builder
+                .HasOne(t => t.SecondaryKitColor)
+                .WithMany(sk => sk.SecondaryKitTeams)
+                .HasForeignKey(t => t.SecondaryKitColorId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Property(t => t.TownId)
+                .IsRequired(true);
+
+            builder
+                .HasOne(t => t.Town)
+                .WithMany(tw => tw.Teams)
+                .HasForeignKey(t => t.TownId);
+        }
+    }
+}
