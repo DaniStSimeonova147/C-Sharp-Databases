6f508c11a7a9344c683239778d46154dc09f115f
Dani Simeonova <danisimeonova147@gmail.com>
2023-11-18 11:07:49 +0200

    Add bet configuration

---
+++ Entity Framework Core/05.ENTITY RELATIONS/P03_FootballBetting/Data/Models/Configuration/BetConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using P03_FootballBetting.Data.Models;
+
+
+namespace P03_FootballBetting.Data.Configurations
+{
+    public class BetConfiguration : IEntityTypeConfiguration<Bet>
+    {
+        public void Configure(EntityTypeBuilder<Bet> builder)
+        {
+            builder.HasKey(b => b.BetId);
+
+            builder.Property(b => b.Amount)
+                .IsRequired(true);
+
+            builder.Property(b => b.Prediction)
+                .IsRequired(true);
+
+            builder.Property(b => b.DateTime)
+                .IsRequired(true);
+
+            builder.Property(b => b.UserId)
+                .IsRequired(true);
+
+            builder
+                .HasOne(b => b.User)
+                .WithMany(u => u.Bets)
+                .HasForeignKey(b => b.UserId);
+
+            builder.Property(b => b.GameId)
+                .IsRequired(true);
+
+            builder
+                .HasOne(b => b.Game)
+                .WithMany(g => g.Bets)
+                .HasForeignKey(b => b.GameId);
+        }
+    }
+}
