993f0b90a9cc6bb33c3004a82cd50cf21e04d008
Dani Simeonova <danisimeonova147@gmail.com>
2023-11-18 11:10:27 +0200

    Add game configuration

---
+++ Entity Framework Core/05.ENTITY RELATIONS/P03_FootballBetting/Data/Models/Configuration/GameConfigurations.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using P03_FootballBetting.Data.Models;
+
+
+namespace P03_FootballBetting.Data.Configurations
+{
+    public class GameConfigurations : IEntityTypeConfiguration<Game>
+    {
+        public void Configure(EntityTypeBuilder<Game> builder)
+        {
+            builder.HasKey(g => g.GameId);
+
+            builder.Property(g => g.HomeTeamGoals)
+                .IsRequired(true);
+
+            builder.Property(g => g.AwayTeamGoals)
+                .IsRequired(true);
+
+            builder.Property(g => g.DateTime)
+                .IsRequired(true);
+
+            builder.Property(g => g.HomeTeamBetRate)
+                .IsRequired(true);
+
+            builder.Property(g => g.AwayTeamBetRate)
+                .IsRequired(true);
+
+            builder.Property(g => g.DrawBetRate)
+                .IsRequired(true);
+
+            builder.Property(g => g.Result)
+                .IsRequired(true);
+
+            builder.Property(g => g.HomeTeamId)
+                .IsRequired(true);
+
+            builder
+                .HasOne(g => g.HomeTeam)
+                .WithMany(t => t.HomeGames)
+                .HasForeignKey(g => g.HomeTeamId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Property(g => g.AwayTeamId)
+                .IsRequired(true);
+
+            builder
+                .HasOne(g => g.AwayTeam)
+                .WithMany(t => t.AwayGames)
+                .HasForeignKey(g => g.AwayTeamId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
