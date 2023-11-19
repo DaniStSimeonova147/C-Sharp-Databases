6eb17f01f2c3a825d90c4e56c23fe5a0f0f4a527
Dani Simeonova <danisimeonova147@gmail.com>
2023-11-19 20:06:34 +0200

    Advanced querying

---
+++ Entity Framework Core/06.ADVANCED QUERYING/BookShop.Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString => @"Server=LAPTOP-SELJOP4P\SQLEXPRESS;Database=BookShop;Integrated Security=True;";
+    }
+}
