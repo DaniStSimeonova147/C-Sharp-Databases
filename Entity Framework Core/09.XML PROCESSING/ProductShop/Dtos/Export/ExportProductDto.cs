914ad3dafc3807635b9c6037abb57284d5f1d5c3
Dani Simeonova <danisimeonova147@gmail.com>
2023-11-22 19:54:41 +0200

    Add export Dtos

---
+++ Entity Framework Core/09.XML PROCESSING/ProductShop/Dtos/Export/ExportProductDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Serialization;
+
+namespace ProductShop.Dtos.Export
+{
+    [XmlType("SoldProducts")]
+   public class ExportProductDto
+    {
+        [XmlElement("count")]
+        public int Count { get; set; }
+
+        [XmlArray("products")]
+        public ExportSoldProductsDto[] Products { get; set; }
+    }
+}
