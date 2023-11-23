c86e3bb7abb42d35dd142a439ed431d8f1b6350c
Dani Simeonova <danisimeonova147@gmail.com>
2023-11-23 19:28:01 +0200

    Add import Dto

---
+++ Entity Framework Core/10.Entity-Framework-Core-Exams/01.TeisterMask-Exam/DataProcessor/ImportDto/ImportEmployeeWithTaskDto.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TeisterMask.DataProcessor.ImportDto
+{
+    public class ImportEmployeeWithTaskDto
+    {
+        [RegularExpression(@"^[A-Za-z0-9]+$"), MinLength(3), MaxLength(40), Required]
+        public string Username { get; set; }
+
+        [EmailAddress, Required]
+        public string Email { get; set; }
+
+        [RegularExpression(@"^[0-9]{3}-[0-9]{3}-[0-9]{4}$"), Required]
+        public string Phone { get; set; }
+
+        [JsonProperty("Tasks")]
+        public int[] Tasks { get; set; }
+    }
+}
