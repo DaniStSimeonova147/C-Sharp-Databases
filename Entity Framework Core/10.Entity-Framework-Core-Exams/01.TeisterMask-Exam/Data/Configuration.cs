43d91f9456b6fb22cbd2d8d84e4e8862cf18b275
Dani Simeonova <danisimeonova147@gmail.com>
2023-11-23 19:26:17 +0200

    Add configuration

---
+++ Entity Framework Core/10.Entity-Framework-Core-Exams/01.TeisterMask-Exam/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace TeisterMask.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=LAPTOP-SELJOP4P\SQLEXPRESS;Database=TeisterMask;Trusted_Connection=True";
+    }
+}
