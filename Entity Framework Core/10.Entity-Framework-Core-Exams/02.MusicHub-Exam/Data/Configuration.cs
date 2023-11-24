256dd1ac63a0630cd1e3023de5a36ed35d1abaa7
Dani Simeonova <danisimeonova147@gmail.com>
2023-11-24 08:46:03 +0200

    Add data config

---
+++ Entity Framework Core/10.Entity-Framework-Core-Exams/02.MusicHub-Exam/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace MusicHub.Data
+{
+   public static class Configuration
+    {
+        public static string ConnectionString =
+            @"Server=LAPTOP-SELJOP4P\SQLEXPRESS;Database=MusicHub;Trusted_Connection=True";
+    }
+}
