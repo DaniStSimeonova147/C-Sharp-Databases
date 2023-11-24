5edc3fad8d4ab01f469f1f4fca5c15142aebec4a
Dani Simeonova <danisimeonova147@gmail.com>
2023-11-24 08:52:17 +0200

    Add import performer Dto

---
+++ Entity Framework Core/10.Entity-Framework-Core-Exams/02.MusicHub-Exam/DataProcessor/ImportDtos/ImportPerformerDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace MusicHub.DataProcessor.ImportDtos
+{
+    [XmlType("Performer")]
+    public class ImportPerformerDto
+    {
+        [XmlElement("FirstName")]
+        [MinLength(3), MaxLength(20), Required]
+        public string FirstName { get; set; }
+
+        [XmlElement("LastName")]
+        [MinLength(3), MaxLength(20), Required]
+        public string LastName { get; set; }
+
+        [XmlElement("Age")]
+        [Range(18, 70), Required]
+        public int Age { get; set; }
+
+        [XmlElement("NetWorth")]
+        [Range(0, double.MaxValue), Required]
+        public decimal NetWorth { get; set; }
+
+        [XmlArray("PerformersSongs")]
+        public ImportPerformerSongsDto[] PerformersSongs { get; set; }
+    }
+}
