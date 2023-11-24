d591ef800bbdf70d8d7fe4a25232f39efcff5e35
Dani Simeonova <danisimeonova147@gmail.com>
2023-11-24 08:49:00 +0200

    Add import song Dto

---
+++ Entity Framework Core/10.Entity-Framework-Core-Exams/02.MusicHub-Exam/DataProcessor/ImportDtos/ImportSongDto.cs
@@ -0,0 +1,43 @@
+﻿using MusicHub.Data.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace MusicHub.DataProcessor.ImportDtos
+{
+    [XmlType("Song")]
+    public class ImportSongDto
+    {
+        [XmlElement("Name")]
+        [MinLength(3), MaxLength(20), Required]
+        public string Name { get; set; }
+
+        [XmlElement("Duration")]
+        [Required]
+        public string Duration { get; set; }
+
+        [XmlElement("CreatedOn")]
+        [Required]
+        public string CreatedOn { get; set; }
+
+        [XmlElement("Genre")]
+        [Required]
+        public string Genre { get; set; }
+
+        [XmlElement("AlbumId")]
+        public int? AlbumId { get; set; }
+
+        [XmlElement("WriterId")]
+        [Required]
+        public int WriterId { get; set; }
+
+        [XmlElement("Price")]
+        [Range(0, double.MaxValue), Required]
+        public decimal Price { get; set; }
+
+       
+    }
+}
