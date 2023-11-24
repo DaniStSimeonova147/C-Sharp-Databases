7d143a2a9da6425cb44d67144c1d32b53f0b3e65
Dani Simeonova <danisimeonova147@gmail.com>
2023-11-24 08:44:00 +0200

    Add performer model

---
+++ Entity Framework Core/10.Entity-Framework-Core-Exams/02.MusicHub-Exam/Data/Models/Performer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MusicHub.Data.Models
+{
+    public class Performer
+    {
+        public Performer()
+        {
+            this.PerformerSongs = new HashSet<SongPerformer>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [MinLength(3), MaxLength(20), Required]
+        public string FirstName { get; set; }
+
+        [MinLength(3), MaxLength(20), Required]
+        public string LastName { get; set; }
+
+        [Range(18, 70), Required]
+        public int Age { get; set; }
+
+        [Range(0, double.MaxValue), Required]
+        public decimal NetWorth  { get; set; }
+
+        public ICollection<SongPerformer> PerformerSongs  { get; set; }
+    }
+}
