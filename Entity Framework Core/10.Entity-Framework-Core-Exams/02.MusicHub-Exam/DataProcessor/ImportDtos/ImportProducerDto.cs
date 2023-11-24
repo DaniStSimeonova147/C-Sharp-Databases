142438d2316264a98c5227d2f21807f0dd6a3e60
Dani Simeonova <danisimeonova147@gmail.com>
2023-11-24 08:51:26 +0200

    Add import producer Dto

---
+++ Entity Framework Core/10.Entity-Framework-Core-Exams/02.MusicHub-Exam/DataProcessor/ImportDtos/ImportProducerDto.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MusicHub.DataProcessor.ImportDtos
+{
+    public class ImportProducerDto
+    {
+        [MinLength(3), MaxLength(30), Required]
+        public string Name { get; set; }
+
+        [RegularExpression(@"^[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+$")]
+        public string Pseudonym { get; set; }
+
+        [RegularExpression(@"^\+359 [0-9]{3} [0-9]{3} [0-9]{3}$")]
+        public string PhoneNumber { get; set; }
+
+        [JsonProperty("Albums")]
+        public ICollection<ImportAlbumsProducerDto> AlbumDtos { get; set; }
+    }
+}
