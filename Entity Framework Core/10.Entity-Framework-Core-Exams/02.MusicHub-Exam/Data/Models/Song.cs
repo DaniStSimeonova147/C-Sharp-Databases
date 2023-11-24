3e68bfeb6895141df1dba733e12af5463aeabcf9
Dani Simeonova <danisimeonova147@gmail.com>
2023-11-24 08:44:52 +0200

    Add song model

---
+++ Entity Framework Core/10.Entity-Framework-Core-Exams/02.MusicHub-Exam/Data/Models/Song.cs
@@ -0,0 +1,46 @@
+﻿using MusicHub.Data.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace MusicHub.Data.Models
+{
+   public class Song
+    {
+        public Song()
+        {
+            this.SongPerformers = new HashSet<SongPerformer>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [MinLength(3), MaxLength(20), Required]
+        public string Name { get; set; }
+
+        [Required]
+        public TimeSpan Duration  { get; set; }
+        
+        [Required]
+        public DateTime CreatedOn  { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+        [Range(0, double.MaxValue), Required]
+        public decimal Price { get; set; }
+
+        [ForeignKey(nameof(Album))]
+        public int? AlbumId { get; set; }
+        public Album Album { get; set; }
+
+        [ForeignKey(nameof(Writer)), Required]
+        public int WriterId  { get; set; }
+        public Writer Writer { get; set; }
+
+
+        public ICollection<SongPerformer> SongPerformers  { get; set; }
+    }
+}
