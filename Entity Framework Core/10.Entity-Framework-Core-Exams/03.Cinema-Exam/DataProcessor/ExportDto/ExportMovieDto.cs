dcd3439d25c5ab728ae49fd1ea81644340fee03b
Dani Simeonova <danisimeonova147@gmail.com>
2023-11-25 20:25:04 +0200

    Add export Dto

---
+++ Entity Framework Core/10.Entity-Framework-Core-Exams/03.Cinema-Exam/DataProcessor/ExportDto/ExportMovieDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cinema.DataProcessor.ExportDto
+{
+    public class ExportMovieDto
+    {
+        public string MovieName { get; set; }
+
+        public string Rating { get; set; }
+
+        public string TotalIncomes { get; set; }
+
+        public ICollection<ExportMovieCustomersDto> Customers { get; set; }
+    }
+}
