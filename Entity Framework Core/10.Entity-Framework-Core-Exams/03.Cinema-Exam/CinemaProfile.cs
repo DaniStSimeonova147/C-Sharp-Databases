8ab40101cf35c362619cd2bd1ecd3ba7828ed389
Dani Simeonova <danisimeonova147@gmail.com>
2023-11-25 20:29:17 +0200

    Add cinema profile

---
+++ Entity Framework Core/10.Entity-Framework-Core-Exams/03.Cinema-Exam/CinemaProfile.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Cinema.Data.Models;
+using Cinema.Data.Models.Enums;
+using Cinema.DataProcessor.ImportDto;
+using System;
+using System.Globalization;
+
+namespace Cinema
+{
+    public class CinemaProfile : Profile
+    {
+        // Configure your AutoMapper here if you wish to use it. If not, DO NOT DELETE THIS CLASS
+        public CinemaProfile()
+        {
+            this.CreateMap<ImportMovieDto, Movie>();
+            //.ForMember(x => x.Genre, y => y.MapFrom(
+            //    x => Enum.Parse(typeof(Genre), x.Genre)))
+            //.ForMember(x => x.Duration, y => y.MapFrom(
+            //    x => TimeSpan.ParseExact(x.Duration, @"hh\:mm\:ss", CultureInfo.InvariantCulture)));
+
+            this.CreateMap<ImportHallWithSeatsDto, Hall>();
+
+            this.CreateMap<ImportProjectionDto, Projection>()
+                .ForMember(x => x.DateTime, y => y.MapFrom(
+                    p => DateTime.ParseExact(p.DateTime, @"yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture)));
+
+            this.CreateMap<ImportTicketDto, Ticket>();
+            this.CreateMap<ImportCustomerDto, Customer>();
+        }
+    }
+}
