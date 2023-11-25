9834eb2776caf343580985f1e391d2395ec70b17
Dani Simeonova <danisimeonova147@gmail.com>
2023-11-25 20:24:02 +0200

    Add config

---
+++ Entity Framework Core/10.Entity-Framework-Core-Exams/03.Cinema-Exam/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Cinema.Data
+{
+   public static class Configuration
+    {
+        public static string ConnectionString = @"Server=LAPTOP-SELJOP4P\SQLEXPRESS;Database=Cinema;Trusted_Connection=True";
+    }
+}
