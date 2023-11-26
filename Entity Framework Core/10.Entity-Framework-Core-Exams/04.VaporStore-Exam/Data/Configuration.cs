6d5d864d05cab5cb0126c0d7e2ee02ec79d81da5
Dani Simeonova <danisimeonova147@gmail.com>
2023-11-26 19:56:56 +0200

    Add config

---
+++ Entity Framework Core/10.Entity-Framework-Core-Exams/04.VaporStore-Exam/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace VaporStore.Data
+{
+	public static class Configuration
+	{
+		public static string ConnectionString =
+            @"Server=LAPTOP-SELJOP4P\SQLEXPRESS;Database=VaporStore;Trusted_Connection=True";
+	}
+}
