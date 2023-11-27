fbbb33e40088ac69bec34eab325098bc0e4d7d7a
Dani Simeonova <danisimeonova147@gmail.com>
2023-11-27 19:19:23 +0200

    Add bonus

---
+++ Entity Framework Core/10.Entity-Framework-Core-Exams/04.VaporStore-Exam/DataProcessor/Bonus.cs
@@ -0,0 +1,31 @@
+﻿namespace VaporStore.DataProcessor
+{
+	using System;
+    using System.Linq;
+    using Data;
+    using VaporStore.Data.Models;
+
+    public static class Bonus
+	{
+		public static string UpdateEmail(VaporStoreDbContext context, string username, string newEmail)
+		{
+            User user = context.Users.FirstOrDefault(u => u.Username == username);
+
+            if (user == null)
+            {
+                return $"User {username} not found";
+            }
+            else if (context.Users.FirstOrDefault(u => u.Email == newEmail) != null)
+            {
+                return $"Email {newEmail} is already taken";
+            }
+
+            user.Email = newEmail;
+            context.Users.Update(user);
+            context.SaveChanges();
+
+            return $"Changed {username}'s email successfully";
+
+        }
+    }
+}
