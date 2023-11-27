42b44b7bc5c7460ab25508b7a9ac03181231eb22
Dani Simeonova <danisimeonova147@gmail.com>
2023-11-27 19:21:35 +0200

    Add Vapor store profile

---
+++ Entity Framework Core/10.Entity-Framework-Core-Exams/04.VaporStore-Exam/VaporStoreProfile.cs
@@ -0,0 +1,23 @@
+﻿namespace VaporStore
+{
+	using AutoMapper;
+    using System;
+    using System.Globalization;
+    using VaporStore.Data.Models;
+    using VaporStore.Data.Models.Enum;
+    using VaporStore.DataProcessor.ImportDtos;
+
+    public class VaporStoreProfile : Profile
+	{
+		// Configure your AutoMapper here if you wish to use it. If not, DO NOT DELETE THIS CLASS
+		public VaporStoreProfile()
+		{
+            this.CreateMap<ImportUserDto, User>();
+            this.CreateMap<ImportCardDto, Card>()
+                .ForMember(x => x.Cvc, y => y.MapFrom(
+                           c => c.CVC))
+                .ForMember(x => x.Type, y => y.MapFrom(
+                           c => Enum.Parse(typeof(CardType), c.Type)));
+		}
+	}
+}
