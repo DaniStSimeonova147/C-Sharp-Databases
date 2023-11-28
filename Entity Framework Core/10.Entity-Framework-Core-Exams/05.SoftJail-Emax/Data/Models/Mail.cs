e530caa0805560e7fabbd49cbf6432029d82e632
Dani Simeonova <danisimeonova147@gmail.com>
2023-11-28 20:03:46 +0200

    Add Mail

---
+++ Entity Framework Core/10.Entity-Framework-Core-Exams/05.SoftJail-Emax/Data/Models/Mail.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace SoftJail.Data.Models
+{
+    public class Mail
+    {
+        //•	Id – integer, Primary Key
+        //•	Description– text(required)
+        //•	Sender – text(required)
+        //•	Address – text, consisting only of letters, spaces and numbers, which ends with “ str.” (required) (Example: “62 Muir Hill str.“)
+        //•	PrisonerId - integer, foreign key
+        //•	Prisoner – the mail's Prisoner (required)
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Sender { get; set; }
+
+        [RegularExpression(@"^[A-Za-z 0-9]+ str.$"), Required]
+        public string Address { get; set; }
+
+        [ForeignKey(nameof(Prisoner)), Required]
+        public int PrisonerId { get; set; }
+        public Prisoner Prisoner { get; set; }
+    }
+}
