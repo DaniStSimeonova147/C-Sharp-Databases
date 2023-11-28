76049413faca5d21c330cef962bbcd257d878a6d
Dani Simeonova <danisimeonova147@gmail.com>
2023-11-28 20:04:48 +0200

    Add config

---
+++ Entity Framework Core/10.Entity-Framework-Core-Exams/05.SoftJail-Emax/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace SoftJail.Data
+{
+   public static class Configuration
+    {
+        public static string ConnectionString = @"Server=LAPTOP-SELJOP4P\SQLEXPRESS;Database=SoftJail;Trusted_Connection=True";
+    }
+}
